31d40988694db57eb768f303889d6aa3c010c558
ace19-dev <acemc19@gmail.com>
2019-02-18 17:21:53 +0900

    update

--- data.py
+++ data.py
@@ -77,8 +77,8 @@ class Dataset(object):
         image = tf.image.random_contrast(image, lower=0.7, upper=1.0)
         image = tf.image.random_hue(image, max_delta=0.08)
         image = tf.image.random_saturation(image, lower=0.7, upper=1.0)
-        # TODO: tf.pad ?
-        image = tf.image.resize_images(image, [self.resize_h, self.resize_w], method=2)
+        # TODO: tf.pad ??
+        image = tf.image.resize_images(image, [self.resize_h, self.resize_w])
 
         return image, label
 
