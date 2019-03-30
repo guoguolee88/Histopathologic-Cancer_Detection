2a72b5abe2a920e5228b81d4c84c9a99979066f3
Sean <acemc19@gmail.com>
2019-03-31 00:05:29 +0900

    update

--- utils/aug_utils.py
+++ utils/aug_utils.py
@@ -46,25 +46,30 @@ def aug(images_or_image):
     # ], random_order=True)
 
     seq = iaa.Sequential([
+        # # Applies either Fliplr or Flipud to images.
+        # iaa.SomeOf(1, [
+        #     iaa.Fliplr(1.0),
+        #     iaa.Flipud(1.0)
+        # ]),
         iaa.Fliplr(0.5), # horizontally flip 50% of the images
         iaa.Flipud(0.2),  # vertically flip 20% of all images
         # Rotates all images by 90 or 270 degrees.
         # Resizes all images afterwards to keep the size that they had before augmentation.
         # This may cause the images to look distorted.
-        iaa.Sometimes(iaa.Rot90((1, 3))),
+        iaa.Sometimes(0.6, iaa.Rot90((1, 3))),
         # crop some of the images by 0-10% of their height/width
         iaa.Crop(percent=(0.4, 0.5)),
-        iaa.Sometimes(
+        iaa.Sometimes(0.6,
             iaa.Affine(
                 scale={"x": (0.7, 1.3), "y": (0.7, 1.3)},
                 translate_percent={"x": (-0.2, 0.2), "y": (-0.2, 0.2)},
                 # rotate=(-45, 45),
                 shear=(-8, 8)
             )),
-        # Execute 0 to 5 of the following (less important) augmenters per
+        # Execute 0 to 3 of the following (less important) augmenters per
         # image. Don't execute all of them, as that would often be way too
         # strong.
-        iaa.SomeOf((0, 5),
+        iaa.SomeOf((0, 3),
            [
                # # Small gaussian blur with random sigma between 0 and 0.5.
                # # But we only blur about 50% of all images.
@@ -88,7 +93,7 @@ def aug(images_or_image):
                # Same as sharpen, but for an embossing effect.
                iaa.Emboss(alpha=(0, 1.0), strength=(0, 2.0)),
                # Strengthen or weaken the contrast in each image.
-               iaa.NContrastNormalization((0.85, 1.5)),
+               iaa.ContrastNormalization((0.85, 1.5)),
                # Add gaussian noise.
                # For 50% of all images, we sample the noise once per pixel.
                # For the other 50% of all images, we sample the noise per pixel AND
