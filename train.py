2edcf9c03a83e544632a58e22057fcd54151cf75
ace19-dev <acemc19@gmail.com>
2019-04-03 13:14:58 +0900

    update hyper-params

--- train.py
+++ train.py
@@ -37,7 +37,7 @@ flags.DEFINE_string('summaries_dir', './models/train_logs',
 
 flags.DEFINE_enum('learning_policy', 'poly', ['poly', 'step'],
                   'Learning rate policy for training.')
-flags.DEFINE_float('base_learning_rate', .0003,
+flags.DEFINE_float('base_learning_rate', .0005,
                    'The base learning rate for model training.')
 flags.DEFINE_float('learning_rate_decay_factor', 1e-4,
                    'The rate to decay the base learning rate.')
