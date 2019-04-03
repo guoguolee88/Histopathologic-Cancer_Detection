3c0857f4469fd84a920faa9602d29c0e08251318
ace19-dev <acemc19@gmail.com>
2019-04-03 13:47:46 +0900

    update aug conf

--- utils/aug_utils.py
+++ utils/aug_utils.py
@@ -6,36 +6,40 @@ def aug(images):
 
     seq = iaa.Sequential([
         # # Applies either Fliplr or Flipud to images.
-        iaa.SomeOf(1, [
-            iaa.Fliplr(0.5),
-            iaa.Flipud(0.5)
+        iaa.SomeOf(2, [
+            iaa.Fliplr(1),
+            iaa.Flipud(1),
+            # Rotates all images by 90, 180 or 270 degrees.
+            # Resizes all images afterwards to keep the size that they had before augmentation.
+            # This may cause the images to look distorted.
+            iaa.Sometimes(0.5, iaa.Rot90((1, 3))),
         ]),
         # iaa.Fliplr(0.5), # horizontally flip 50% of the images
         # iaa.Flipud(0.2),  # vertically flip 20% of all images
 
         # crop some of the images by 0-30% of their height/width
-        iaa.Crop(percent=(0, 0.3)),
+        iaa.Crop(percent=(0, 0.2)),
 
-        # Rotates all images by 90, 180 or 270 degrees.
-        # Resizes all images afterwards to keep the size that they had before augmentation.
-        # This may cause the images to look distorted.
-        iaa.Sometimes(0.5, iaa.Rot90((1, 3))),
+        # # Rotates all images by 90, 180 or 270 degrees.
+        # # Resizes all images afterwards to keep the size that they had before augmentation.
+        # # This may cause the images to look distorted.
+        # iaa.Sometimes(0.5, iaa.Rot90((1, 3))),
 
-        # Apply affine transformations to each image.
-        # Scale/zoom them, translate/move them, rotate them and shear them.
-        iaa.Affine(
-            scale={"x": (0.8, 1.2), "y": (0.8, 1.2)},
-            translate_percent={"x": (-0.2, 0.2), "y": (-0.2, 0.2)},
-            # rotate=(-25, 25),
-            shear=(-8, 8)
-        ),
-        # iaa.Sometimes(0.5,
-        #     iaa.Affine(
-        #         scale={"x": (0.7, 1.3), "y": (0.7, 1.3)},
-        #         translate_percent={"x": (-0.2, 0.2), "y": (-0.2, 0.2)},
-        #         # rotate=(-45, 45),
-        #         shear=(-8, 8)
-        #     )),
+        # # Apply affine transformations to each image.
+        # # Scale/zoom them, translate/move them, rotate them and shear them.
+        # iaa.Affine(
+        #     scale={"x": (0.8, 1.2), "y": (0.8, 1.2)},
+        #     translate_percent={"x": (-0.2, 0.2), "y": (-0.2, 0.2)},
+        #     # rotate=(-25, 25),
+        #     shear=(-8, 8)
+        # ),
+        # # iaa.Sometimes(0.5,
+        # #     iaa.Affine(
+        # #         scale={"x": (0.7, 1.3), "y": (0.7, 1.3)},
+        # #         translate_percent={"x": (-0.2, 0.2), "y": (-0.2, 0.2)},
+        # #         # rotate=(-45, 45),
+        # #         shear=(-8, 8)
+        # #     )),
 
         # # iaa.SomeOf((0, 3),
         # #    [
